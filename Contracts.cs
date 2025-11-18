7b5d36192af95609c211706ea3579ee9c3645a4c
Alexandre Zordan <44484412+sorza@users.noreply.github.com>
2025-11-17 21:17:56 -0300

    Add files via upload

---
+++ Contracts.cs
@@ -0,0 +1,13 @@
+﻿namespace FCG.Shared.Contracts
+{
+    public record GameCreatedEvent(Guid Id, string Title, decimal Price, int LaunchYear, string Developer, string Genre);
+    public record GameUpdatedEvent(Guid Id, string Title, decimal Price, int LaunchYear, string Developer, string Genre);
+    public record GameDeletedEvent(Guid Id);
+
+    public record LibraryOrderEvent(Guid ItemId, Guid UserId, Guid GameId, string Status, decimal? PricePaid);
+    public record LibraryItemCreatedEvent(Guid ItemId, Guid UserId, Guid GameId, string Status, decimal? PricePaid);
+
+    public record UserCreatedEvent(Guid Id, string Name, string Email, string Profile, bool Active);
+    public record UserDeletedEvent(Guid id);
+
+}
