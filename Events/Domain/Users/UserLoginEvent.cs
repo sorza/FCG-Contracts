c087ba9eb9741a6bb8fe49ea0547fc29bd173b80
sorza <alexandre.sorza@outlook.com>
2026-01-07 09:58:37 -0300

    garantindo o timestamp real com init

--- Events/Domain/Users/UserLoginEvent.cs
+++ Events/Domain/Users/UserLoginEvent.cs
@@ -5,7 +5,7 @@ namespace FCG.Shared.Contracts.Events.Domain.Users
 {
     public record UserLoginEvent(string AggregateId, string Name, string Ip, string Device) : IDomainEvent
     {
-        public DateTime OccurredAt { get; } = DateTime.UtcNow;
+        public DateTime OccurredAt { get; init; } = DateTime.UtcNow;
 
     }
 }
