0f38dc9b90f8dd19176fabcad275572daad1003c
sorza <alexandre.sorza@outlook.com>
2026-01-07 15:54:44 -0300

    refresh

--- Events/Domain/Payments/PaymentStatusChanged.cs
+++
@@ -1,9 +0,0 @@
-﻿using FCG.Shared.Contracts.Interfaces;
-
-namespace FCG.Shared.Contracts.Events.Domain.Payments
-{
-    public record PaymentStatusChanged(string AggregateId, string PaymentType, string Status, decimal Price ) : IDomainEvent
-    {       
-        public DateTime OccurredAt { get; init; } = DateTime.UtcNow;
-    }
-}
